8cc3a8762a791f7340a12256dbb741ba5964bd54
ABRARgomaa <abrargomaa111@gmail.com>
2024-07-06 03:16:32 +0300

    up

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+def sum_list
+"""
+
+
+def sum_list(input_list: float) -> float:
+    """
+    def sum_list
+    """
+    sum = 0.0
+    for n in input_list:
+        sum += n
+    return sum
