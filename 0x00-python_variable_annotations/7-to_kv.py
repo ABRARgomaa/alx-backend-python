9f5c0eb7d455b2204acb0e9ed6f00fddc37b7e25
ABRARgomaa <abrargomaa111@gmail.com>
2024-07-06 03:57:14 +0300

    UP

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+def to_kv
+"""
+
+
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    def to_kv
+    """
+    return k, v**2
