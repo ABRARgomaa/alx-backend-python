409f6cf8211cfdd3acf3dec3395bebfacb941e7b
ABRARgomaa <abrargomaa111@gmail.com>
2024-07-09 01:57:12 +0300

    up

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -10,7 +10,7 @@ import asyncio
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def measure_time(n, max_delay) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """
     hi again
     """
