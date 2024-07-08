12916206c1ed381cfa2b24514bcfa5eebdf4b7d8
ABRARgomaa <abrargomaa111@gmail.com>
2024-07-09 02:27:32 +0300

    up

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-hi
+create_task
 """
 
 
@@ -11,7 +11,6 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """
-    hi again
+    create_task
     """
-    task = asyncio.create_task(wait_random(max_delay))
-    return task
+    return asyncio.create_task(wait_random(max_delay))
