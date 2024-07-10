f6044fedc25851d197ea737c5097a89301b821a2
ABRARgomaa <abrargomaa111@gmail.com>
2024-07-10 10:12:58 +0300

    UP

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -4,13 +4,14 @@ async
 """
 
 
+from typing import List
 import asyncio
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """
     async
     """
-    result = [i in]
+    result = [i async for i in async_generator()]
     return(result)
