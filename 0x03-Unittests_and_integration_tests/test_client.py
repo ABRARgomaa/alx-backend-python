675bf169ed610d215af071255e8b1f8da4ee789b
ABRARgomaa <abrargomaa111@gmail.com>
2024-07-29 06:28:21 +0300

    up

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-"""
-client test file
-"""
+""" Module for testing client """
 
 from client import GithubOrgClient
+from fixtures import TEST_PAYLOAD
 from parameterized import parameterized, parameterized_class
 import json
 import unittest
